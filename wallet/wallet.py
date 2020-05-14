0880bfc0e765bacb4d46773f40bd881dd7ae8071
Brody <wacker.brody@gmail.com>
2020-05-13 22:36:42 -0400

    adding ethereum solution

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -45,7 +45,7 @@ def priv_key_to_account (coin, priv_key):
         return Account.privateKeyToAccount(priv_key)
     elif coin == BTCTEST:
         return PrivateKeyTestnet(priv_key)
-    
+
 # setting sender ETH account
 eth_account = priv_key_to_account(ETH,coins()[ETH][0]['privkey'])
 
@@ -58,10 +58,8 @@ btctest_account = priv_key_to_account(BTCTEST,coins()[BTCTEST][0]['privkey'])
 # setting a receiver BTCTEST address
 btctest_address = coins()[BTCTEST][1]['address']
 
-# setting up Web3 config
+# setting up Web3 port
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-w3.middleware_onion.inject(geth_poa_middleware, layer =0)
-w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
 # function to create raw, unsigned transaction
 def create_tx(coin, account, to, amount):
@@ -69,16 +67,16 @@ def create_tx(coin, account, to, amount):
         gasEstimate = w3.eth.estimateGas({
             "from": account.address, 
             "to": to, 
-            "value": amount
+            "value": w3.toWei(amount,'ether') 
         })
         return {
             "from": account.address,
             "to": to,
-            "value": amount,
+            "value": w3.toWei(amount,'ether') ,
             "gas": gasEstimate,
             "gasPrice": w3.eth.gasPrice,
             "nonce": w3.eth.getTransactionCount(account.address),
-            "chainID": w3.eth.net.getId()
+            #"chainID": w3.net.chainId --this has been deprecated
         }
     elif coin == BTCTEST:
         return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
@@ -91,6 +89,12 @@ def send_tx(coin, account, recipient, amount):
     
     if coin == ETH:
         result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-        return result.hex()
+        return result
     elif coin == BTCTEST:
         return NetworkAPI.broadcast_tx_testnet(signed_tx)
+
+# sending ETH transaction
+send_tx(ETH,eth_account,eth_address,1)
+
+# sending BTCTEST transaction
+send_tx(BTCTEST,btctest_account,btctest_address,0.001)
